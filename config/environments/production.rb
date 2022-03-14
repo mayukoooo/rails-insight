390b786798f70888dc60d9d6e62d99e22bf7a85f
mayukoooo <yamagishi.work3@gmail.com>
2022-03-14 20:47:48 +0900

    test.html追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,12 +48,12 @@ Rails.application.configure do
   # Prepend all log lines with the following tags.
   config.log_tags = [:request_id]
 
-  # Use a different cache store in production.
+  # Use a different cache store in .
   # config.cache_store = :mem_cache_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "api_production"
+  # config.active_job.queue_name_prefix = "api_"
 
   config.action_mailer.perform_caching = false
 
