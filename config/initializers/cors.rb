c15351fb69e54d3da5f57ff372b53ce375326cd0
mayukoooo <yamagishi.work3@gmail.com>
2022-03-24 00:02:31 +0900

    cors.rb修正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080'
-    origins 'localhost:8080'
-
-
-
+    origins 'localhost:8080', 'https://www.insight-naisei.com'
     resource '*',
              headers: :any,
              methods: %i[get post put patch delete options head]
